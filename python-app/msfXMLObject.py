85103dcd67e09f6fa58af0b4371f38710e4416ec
Mohammad Farhan <mfarhan102@gmail.com>
2013-06-01 16:25:36 -0400

    improved python scripts

--- python-app/msfXMLObject.py
+++ python-app/msfXMLObject.py
@@ -4,8 +4,22 @@ class msfXmlObject(object):
 
     def __init__(self, fileName):
         self.tree = ET.parse(fileName)
-        self.root = tree.getroot()
+        self.root = self.tree.getroot()
     
     def find_item(self, itemName):
-        
+        d = ['empty']
+        for child in self.root:
+            i = 0
+            for key in child.attrib:
+                if itemName == child.attrib[key]:
+                    d.append(child)
+                    continue
+                for child1 in child:
+                    
+                    if i < 1:
+                        if itemName ==  child1.text:
+                            d.append(child)
+                            i += 1
+
+        return d    
 
--- python-app/msf_application.py
+++ python-app/msf_application.py
@@ -1,6 +1,6 @@
 import sys
-from PySide import QtGui
-
+from PySide import QtGui,QtCore
+from msfXMLObject import msfXmlObject
 class MSF_Part_Identifier(QtGui.QWidget):
     
     def __init__(self):
@@ -9,16 +9,33 @@ class MSF_Part_Identifier(QtGui.QWidget):
         self.initUI()
         
     def initUI(self):
+        self.xml_database = msfXmlObject('sample.xml')
         
-        lbl1 = QtGui.QLabel('Part Code', self)
-        lbl1.move(10, 12)
+        self.lbl = QtGui.QLabel('Part Code', self)
+        self.lbl.move(10, 12)
 
-        lbl2 = QtGui.QLineEdit(self)
-        lbl2.move(100, 10)
+        self.txt = QtGui.QLineEdit(self)
+        self.txt.move(100, 10)
         
-        lbl3 = QtGui.QPushButton('Retrieve Part', self)
-        lbl3.move(250, 10)        
+        self.but = QtGui.QPushButton('Retrieve Part', self)
+        self.but.move(250, 10)        
+        self.lstwid = QtGui.QListWidget(self)
+        self.lstwid.move(10,30)
         
+        def find_item(self):
+            dict = self.xml_database.find_item(self.txt.text())
+            self.lstwid.clear()
+            if len(dict) > 1:
+                
+                for item in dict:
+                    i = 0
+                    if item != "empty":
+                        temp_lab = QtGui.QListWidgetItem(item.attrib['name'])
+                        self.lstwid.insertItem(i,temp_lab)
+                        i = i + 1
+        self.but.clicked.connect(lambda: find_item(self))
+        
+   
         self.setGeometry(300, 300, 400, 150)
         self.setWindowTitle('MSF Part Identifier')    
         self.show()
