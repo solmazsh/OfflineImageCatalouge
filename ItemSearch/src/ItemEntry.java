fd9e57767ecc14ce4884222381404013391d1650
Chris Liu <chrisliu123@gmail.com>
2013-06-01 14:44:11 -0400

    Hi.

--- ItemSearch/src/ItemEntry.java
+++ ItemSearch/src/ItemEntry.java
@@ -1,29 +1,45 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
-
-public class ItemEntry extends JFrame {
+public class ItemEntry extends JFrame implements ActionListener {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -1369810694073517702L;
 
-	
-	public static void main(String args[]){
+	public static void main(String args[]) {
 		new ItemEntry();
 	}
-	
-	ItemEntry(){
+
+	ItemEntry() {
 		JMenuBar menuBar = new JMenuBar();
 		JMenu menu = new JMenu("File");
 		JMenuItem openMenuItem = new JMenuItem("Open");
-	    menu.add(openMenuItem);
-	    menuBar.add(menu);
-	    this.setJMenuBar(menuBar);
-	    this.setSize(600,400);
+		openMenuItem.setName("open");
+		openMenuItem.addActionListener(this);
+		menu.add(openMenuItem);
+		menuBar.add(menu);
+		this.setJMenuBar(menuBar);
+		this.setSize(600, 400);
+		this.setLocationRelativeTo(null);
+		this.setTitle("ItemEntry");
 		setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+	    if(event.getSource() instanceof JMenuItem){
+	    	JMenuItem menuItem = (JMenuItem)event.getSource();
+	    	if(menuItem.getName() == "open"){
+	    		//open file
+	    		//System.out.println("open");
+	    	}
+	    }
 	}
 }
