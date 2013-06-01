880a5cefedd1791a5131ea40eff632eb2a56bc64
Chris Liu <chrisliu123@gmail.com>
2013-06-01 13:48:04 -0400

    Hi.

---
+++ ItemSearch/src/ItemEntry.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+
+
+public class ItemEntry extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1369810694073517702L;
+
+	
+	public static void main(String args[]){
+		new ItemEntry();
+	}
+	
+	ItemEntry(){
+		setVisible(true);
+	}
+}
