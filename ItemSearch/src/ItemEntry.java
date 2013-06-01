b91d7a0a85eec841929c653dd7ab0748f9c7ae30
Chris Liu <chrisliu123@gmail.com>
2013-06-01 14:29:36 -0400

    Hi.

--- ItemSearch/src/ItemEntry.java
+++ ItemSearch/src/ItemEntry.java
@@ -1,4 +1,7 @@
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 
 public class ItemEntry extends JFrame {
@@ -14,6 +17,13 @@ public class ItemEntry extends JFrame {
 	}
 	
 	ItemEntry(){
+		JMenuBar menuBar = new JMenuBar();
+		JMenu menu = new JMenu("File");
+		JMenuItem openMenuItem = new JMenuItem("Open");
+	    menu.add(openMenuItem);
+	    menuBar.add(menu);
+	    this.setJMenuBar(menuBar);
+	    this.setSize(600,400);
 		setVisible(true);
 	}
 }
